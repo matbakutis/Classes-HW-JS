f5ab3cc0586a4af6ccc77557d9cde1b1ce7bc1e1
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-09-20 16:18:35 -0500

    Commit 1 - Created Pet Class

---
+++ main.js
@@ -0,0 +1,55 @@
+
+class Pet {
+	constructor(name){
+		this.name = name;
+		this.owner = "";
+	}
+	setOwner(owner){
+		this.owner = owner;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
