0dad25039f6fcd897e976ac207df6daca47168e3
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-09-20 17:12:13 -0500

    Commit 4 - Created Person Class

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ class Dog extends Pet {
 	}
 };
 
-const sparky = new Dog("$250", "Sparky");
+const sparky = new Dog(9, "Sparky");
 sparky.bark();
 sparky.chaseTail();
 console.log(sparky.getPrice());
@@ -48,14 +48,63 @@ class Cat extends Pet {
 };
 
 
-const sprinkles = new Cat("$200", "Sprinkles");
+const sprinkles = new Cat(8, "Sprinkles");
 sprinkles.purr();
 sprinkles.clean();
 console.log(sprinkles.getPrice());
 
 
+class Person {
+	constructor(name){
+		this.name = name;
+		this.age = 0;
+		this.weight = 0;
+		this.mood = 0;
+		this.pets = [];
+		this.bankAccount = 0;
+	}
+	getName(){
+		return this.name;
+	}
+	getAge(){
+		return this.age;
+	}
+	getWeight(){
+		return this.weight;
+	}
+	greet(other_person){
+		console.log("Hey there, " + other_person);
+	}
+	eat(){
+		this.weight++;
+		this.mood++;
+	}
+	exercise(){
+		this.weight--;
+	}
+	cage(){
+		this.age++;
+		this.weight++;
+		this.mood--;
+		this.bankAccount += 10;
+	}
+	buyPet(pet){
+		this.pets.push(pet);
+		this.mood += 10;
+		this.bankAccount -= pet.getPrice();
+	}
+};
 
-
+const jill = new Person("Jill");
+console.log(jill.getName());
+console.log(jill.getAge());
+console.log(jill.getWeight());
+jill.greet("Billy");
+jill.eat();
+jill.exercise();
+jill.cage();
+jill.buyPet(sprinkles);
+console.log(jill);
 
 
 
