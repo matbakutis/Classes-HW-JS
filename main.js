da621d973526dca0cba3edd2a9516eb286022e87
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-09-20 16:45:04 -0500

    Commit 2 - Created Dog Class

--- main.js
+++ main.js
@@ -9,7 +9,26 @@ class Pet {
 	}
 }
 
+class Dog extends Pet {
+	constructor(price, name){
+		super(name);
+		this.price = price;
+	}
+	bark(){
+		console.log("Bark");
+	}
+	chaseTail(){
+		console.log("oh boy oh boy");
+	}
+	getPrice(){
+		return this.price;
+	}
+};
 
+const sparky = new Dog("$250", "Sparky");
+sparky.bark();
+sparky.chaseTail();
+console.log(sparky.getPrice());
 
 
 
